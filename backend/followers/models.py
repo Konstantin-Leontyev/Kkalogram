fe5cafd0445f7bb7687454c5944b0700d2120449
Konstantin Leontyev <K.A.Leontyev@gmail.com>
2024-05-02 11:24:11 +0400

    Add subscribers model (pre test)

---
+++ backend/followers/models.py
@@ -0,0 +1,34 @@
+from django.contrib.auth import get_user_model
+from django.db.models import (CASCADE, CheckConstraint, F, ForeignKey, Model,
+                              Q, UniqueConstraint)
+
+User = get_user_model()
+
+
+class Follow(Model):
+    user = ForeignKey(
+        User,
+        on_delete=CASCADE,
+        related_name='follower',
+        verbose_name='Подписчик'
+    )
+    author = ForeignKey(
+        User,
+        on_delete=CASCADE,
+        related_name='following',
+        verbose_name='Автор'
+    )
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['user', 'author'],
+                name='No duplicate subscription'
+            ),
+            CheckConstraint(
+                check=~Q(author=F("user")),
+                name='No self subscription'
+            ),
+        ]
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,10 +1,18 @@
-# from djoser.views import UserViewSet
-# from rest_framework.filters import SearchFilter
-#
-# from backend.users.serializers import CustomUserCreateSerializer
-#
-#
-# class CustomUserCreateViewSet(UserViewSet):
-#     serializer_class = CustomUserCreateSerializer
-#     filter_backends = (SearchFilter,)
-#     search_fields = ('email', 'username',)
+from djoser.views import UserViewSet
+from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter
+from rest_framework.permissions import IsAuthenticated
+
+from backend.users.serializers import CustomUserSerializer
+
+
+class CustomUserViewSet(UserViewSet):
+    serializer_class = CustomUserSerializer
+    filter_backends = (SearchFilter,)
+    search_fields = ('email', 'username',)
+
+    @action(('GET',), detail=False, permission_classes=(IsAuthenticated,))
+    def me(self, request, *args, **kwargs):
+        """Функция работы с адресом me."""
+        self.get_object = self.get_instance
+        return self.retrieve(request, *args, **kwargs)
