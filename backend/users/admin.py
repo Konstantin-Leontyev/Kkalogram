129f418615035d6d3924a5ccd3e9a215ce096dec
Konstantin Leontyev <K.A.Leontyev@gmail.com>
2024-05-14 15:07:05 +0400

    Remove Gruoup and Token from admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,20 +1,6 @@
-# from django.contrib.admin import ModelAdmin, register
-# from django.contrib.auth import get_user_model
-#
-# User = get_user_model()
-#
-#
-# @register(User)
-# class UserAdmin(ModelAdmin):
-#     """User model admin site registration class."""
-#
-#     list_display = [
-#         'username',
-#         'email',
-#         'is_active',
-#         'is_staff',
-#         'last_login',
-#         'date_joined',
-#     ]
-#     search_fields = ['email', 'username']
-#     list_filter = ['is_active', 'is_staff']
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from rest_framework.authtoken.models import TokenProxy
+
+admin.site.unregister(Group)
+admin.site.unregister(TokenProxy)
