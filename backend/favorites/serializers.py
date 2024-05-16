b7ad3a2b8d913e29e70deff3303eae087b3e0a7c
Konstantin Leontyev <K.A.Leontyev@gmail.com>
2024-05-16 15:49:30 +0400

    Add UserRecipeSerializer into core

--- backend/favorites/serializers.py
+++ backend/favorites/serializers.py
@@ -1,27 +1,11 @@
-from rest_framework.exceptions import ValidationError
-from rest_framework.serializers import ModelSerializer
-
+from core.serializers import UserRecipeSerializer
 from .models import Favorite
-from recipes.serializers import RecipeSerializer
 
 
-class FavoriteSerializer(ModelSerializer):
+class FavoriteSerializer(UserRecipeSerializer):
     """Describes a favorite serializer."""
 
-    class Meta:
+    class Meta(UserRecipeSerializer.Meta):
         """Describes a favorite serializer metaclass."""
 
         model = Favorite
-        fields = ('user', 'recipe')
-
-    def validate(self, data):
-        """Validate the recipe is already in the user’s favorite."""
-        if Favorite.objects.filter(user=data['user'], recipe=data['recipe']):
-            raise ValidationError('Рецепт уже есть в избранном.')
-        return data
-
-    def to_representation(self, instance):
-        """Change serializer to representation."""
-        return RecipeSerializer(instance.recipe, context={
-            'request': self.context.get('request')
-        }).data
