f3dcace2722b95da9667515b2038aa3bd569bd2f
Konstantin Leontyev <K.A.Leontyev@gmail.com>
2024-05-21 21:18:33 +0400

    Add dosc link in ngnix locations

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -113,11 +113,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 6,
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 6,
 }
 
 ROOT_URLCONF = 'foodgram.urls'
