e3b2a1a4559515fb90a7273cf3376899bc74b85c
Konstantin Leontyev <K.A.Leontyev@gmail.com>
2024-05-22 20:25:53 +0400

    add localhost by hand

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_URLS_REGEX = r'^/api/.*$'
-CSRF_TRUSTED_ORIGINS = [os.getenv('CSRF_TRUSTED_ORIGINS', 'default-value')]
+# CSRF_TRUSTED_ORIGINS = [os.getenv('CSRF_TRUSTED_ORIGINS', 'default-value')]
+CSRF_TRUSTED_ORIGINS = ['https://kkalogram.ru', 'http://127.0.0.1']
 
 DEBUG = os.getenv('DEBUG') == 'True'
 
