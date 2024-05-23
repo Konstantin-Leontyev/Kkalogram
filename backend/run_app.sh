c983985aedc575b9ead6fff6051678e55ba51292
Konstantin Leontyev <K.A.Leontyev@gmail.com>
2024-05-23 17:09:01 +0400

    Add databse wait

--- backend/run_app.sh
+++ backend/run_app.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+while ! nc -z db 5432;
+    do sleep .5;
+    echo "wait database";
+done;
+    echo "connected to the database";
+
 python manage.py migrate;
 python manage.py import_csv;
 python manage.py collectstatic --noinput;
-#cp -r /app/collected_static/. /backend_static/static/
 gunicorn -w 2 -b 0:8000 foodgram.wsgi;
