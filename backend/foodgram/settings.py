380e99d42b876a2b97e49f274472f8121a91fe62
Konstantin Leontyev <K.A.Leontyev@gmail.com>
2024-05-24 08:50:07 +0400

    Add white list setting

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -27,9 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # CORS_ORIGIN_ALLOW_ALL = False
 # CORS_URLS_REGEX = r'^/api/.*$'
-# CORS_ORIGIN_WHITELIST = [
-#     'http://localhost:8000'
-# ]
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+]
 
 DEBUG = os.getenv('DEBUG') == 'True'
 
