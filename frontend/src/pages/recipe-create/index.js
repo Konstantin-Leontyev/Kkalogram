273a9a22398085abc2862db735ebbf8b286bd487
Konstantin Leontyev <K.A.Leontyev@gmail.com>
2024-08-17 20:28:48 +0400

    Diplom deploy

--- frontend/src/pages/recipe-create/index.js
+++ frontend/src/pages/recipe-create/index.js
@@ -175,7 +175,7 @@ const RecipeCreate = ({ onEdit }) => {
                 className={styles.ingredientsAddedItem}
               >
                 <span className={styles.ingredientsAddedItemTitle}>{item.name}</span>
-                <span> - {item.amount}{item.measurement_unit}</span>
+                <span> - {item.amount} {item.measurement_unit}</span>
                 <span
                   className={styles.ingredientsAddedItemRemove}
                   onClick={_ => {
